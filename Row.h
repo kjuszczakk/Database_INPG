56a7f49bb87964dd9afbdd1ede102c71e14e0fd2
kjuszczakk <38715705+kjuszczakk@users.noreply.github.com>
2018-05-13 21:54:44 +0200

    Rozpoczęcie implementacji pliku nagłówkowego.

---
+++ Row.h
@@ -0,0 +1,15 @@
+#ifndef ROW_HPP_
+#define ROW_HPP_
+#include <string>
+
+class Row{
+public:
+    Row(std::string name,int product_size = 0) :
+    void addData(std::string name,int product_size);
+private:
+
+
+};
+
+
+#endif // ROW_HPP_
