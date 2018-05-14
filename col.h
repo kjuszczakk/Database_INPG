4a29c9b12b643a2ddb9d3675e312bb229ce335a0
kubuard <38730059+kubuard@users.noreply.github.com>
2018-05-14 13:05:37 +0200

    Lista inicjalizacyjana

--- col.h
+++ col.h
@@ -1,4 +1,20 @@
 #ifndef COL_H
 #define COL_H
+#include <string>
+
+class Column{
+public:
+    Column(std::string name, int nrows): _name(name), _nrows(nrows){}
+    
+    
+    
+private:
+    std::string _name;
+    int nrows;
+    
+}
+
+
+
 
 #endif // COL_H
