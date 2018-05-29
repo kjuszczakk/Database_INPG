9b4d389b409ebb29343b82da7c11feaf7bbda892
kjuszczakk <38715705+kjuszczakk@users.noreply.github.com>
2018-05-29 17:18:40 +0200

    Stworzenie połączenia z bazą danych

--- SQLDataAccessDemo/FormUI/Helper.cs
+++ SQLDataAccessDemo/FormUI/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace FormUI
 {
     public static class Helper
     {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
     }
 }
